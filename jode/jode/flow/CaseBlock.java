487964a05f2790d99f5b35a245abe0a5daa5c50e
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1998-09-26 17:22:07 +0000

    Initial revision

---
+++ jode/jode/flow/CaseBlock.java
@@ -0,0 +1,108 @@
+/* CaseBlock Copyright (C) 1997-1998 Jochen Hoenicke.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+package jode.flow;
+
+/** 
+ * This block represents a case instruction.  A case instruction is a
+ * part of a switch construction and may have a subpart or not (for
+ * multiple case directly after each other.
+ *
+ * @author Jochen Hoenicke */
+public class CaseBlock extends StructuredBlock {
+    /**
+     * The inner block that jumps to the subroutine, or null
+     * if this is a value only.
+     */
+    StructuredBlock subBlock;
+
+    /**
+     * The value of this case.
+     */
+    int value;
+
+    /**
+     * True, if this is the default case
+     */
+    boolean isDefault = false;
+
+    /**
+     * True, if this is the last case in a switch
+     */
+    boolean isLastBlock;
+
+    /**
+     * The type of the switch value.
+     */
+    sun.tools.java.Type type;
+
+    public CaseBlock(int value) {
+	this.value = value;
+	subBlock = null;
+    }
+
+    public CaseBlock(int value, Jump dest) {
+	this.value = value;
+	subBlock = new EmptyBlock(dest);
+	subBlock.outer = this;
+    }
+
+    /**
+     * Replaces the given sub block with a new block.
+     * @param oldBlock the old sub block.
+     * @param newBlock the new sub block.
+     * @return false, if oldBlock wasn't a direct sub block.
+     */
+    public boolean replaceSubBlock(StructuredBlock oldBlock, 
+				   StructuredBlock newBlock) {
+        if (subBlock == oldBlock)
+            subBlock = newBlock;
+        else
+            return false;
+        return true;
+    }
+
+    /**
+     * Returns all sub block of this structured block.
+     */
+    public StructuredBlock[] getSubBlocks() {
+	if (subBlock != null) {
+	    StructuredBlock[] result = { subBlock };
+	    return result;
+	}
+	return new StructuredBlock[0];
+    }
+
+    public void dumpInstruction(jode.TabbedPrintWriter writer) 
+        throws java.io.IOException 
+    {
+	if (isDefault) {
+	    if (isLastBlock
+		&& subBlock instanceof EmptyBlock
+		&& subBlock.jump == null)
+		return;
+	    writer.println("default:");
+	} else
+	    writer.println("case " + value /*XXX-type*/ + ":");
+	if (subBlock != null) {
+	    writer.tab();
+	    subBlock.dumpSource(writer);
+	    writer.untab();
+	}
+    }
+}
---
+++ jode/jode/flow/RetBlock.java
@@ -0,0 +1,53 @@
+/* RetBlock Copyright (C) 1997-1998 Jochen Hoenicke.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+package jode.flow;
+import jode.LocalInfo;
+
+/** 
+ * This block represents a ret instruction.  A ret instruction is
+ * used to call the finally block, or to call the monitorexit block in
+ * a synchronized block.
+ *
+ * @author Jochen Hoenicke
+ */
+public class RetBlock extends StructuredBlock {
+    /**
+     * The local containing the return address
+     */
+    LocalInfo local;
+
+    public RetBlock(LocalInfo local) {
+	this.local = local;
+	used.addElement(local);
+    }
+
+    /**
+     * Fill all in variables into the given VariableSet.
+     * @param in The VariableSet, the in variables should be stored to.
+     */
+    public void fillInSet(VariableSet in) {
+	in.addElement(local);
+    }
+
+    public void dumpInstruction(jode.TabbedPrintWriter writer) 
+        throws java.io.IOException 
+    {
+	writer.println("RET "+local);
+    }
+}
