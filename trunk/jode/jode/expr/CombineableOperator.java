866cc3b407a8a5d4a57e7adbbc4ebfd0803188a7
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-02-05 12:48:06 +0000

    Initial revision

---
+++ trunk/jode/jode/expr/CombineableOperator.java
@@ -0,0 +1,32 @@
+/* 
+ * CombineableOperator (c) 1998 Jochen Hoenicke
+ *
+ * You may distribute under the terms of the GNU General Public License.
+ *
+ * IN NO EVENT SHALL JOCHEN HOENICKE BE LIABLE TO ANY PARTY FOR DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JOCHEN HOENICKE 
+ * HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JOCHEN HOENICKE SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+ * BASIS, AND JOCHEN HOENICKE HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * $Id$
+ */
+package jode.decompiler;
+
+public interface CombineableOperator {
+    /**
+     * Checks if the loadOp is combinable with this operator.
+     */
+    public boolean matches(Operator loadOp);
+
+    /**
+     * Make this operator return a value compatible with the loadOp
+     * that it should replace.
+     */
+    public void makeNonVoid();
+}
