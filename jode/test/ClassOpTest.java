293a6f707bdb7b0b189e5099d8712683b7c16981
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-02-12 15:35:06 +0000

    *** empty log message ***

--- jode/test/ClassOpTest.java
+++ jode/test/ClassOpTest.java
@@ -4,7 +4,7 @@ public class ClassOpTest {
     static void test1() {
 	Class c1 = ClassOpTest.class;
 	Class c2 = Object.class;
-	Class c3 = ClassOpTest.class;
+	if (ClassOpTest.class == null);
 	c1.getClass();
     }
 
