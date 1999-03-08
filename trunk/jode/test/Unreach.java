b57fc6b5397e7d73c2e5c5579df8ff105ee31fd2
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-03-08 18:03:41 +0000

    Initial revision

---
+++ trunk/jode/test/Unreach.java
@@ -0,0 +1,29 @@
+package jode.test;
+
+/* A test class submitted by dave@onekiwi.demon.co.uk */
+class Unreach
+{
+	static int j = 0;
+	final double[] d = {0.5, 0.4, 0.3};	// won't decompile
+
+    public static final void m(int i) throws Exception {
+	switch (i) {
+	case 1:
+	    j += 2;
+	        for (;;) {
+		        j += 3;
+		        switch (j) {
+		        case 2:
+		            break;
+		        default:
+		            j += 4;
+		            return;
+		        }
+	        }
+	        // An unreachable break is inserted here
+	default:
+	    j += 5;	// decompiles as j = j + 1;  -- not quite optimal
+	}
+    }
+}
+
