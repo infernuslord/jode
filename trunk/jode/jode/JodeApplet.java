7045e9e9298e965cc59d45c3c5309c70f3f9a370
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-03-11 22:47:47 +0000

    awt10 code eingebettet

--- trunk/jode/jode/JodeApplet.java
+++ trunk/jode/jode/JodeApplet.java
@@ -10,6 +10,14 @@ public class JodeApplet extends Applet {
     public JodeApplet() {
 	jodeWin = new JodeWindow(this);
     }
+
+
+///#ifdef AWT10
+///    public boolean action(Event e, Object arg) {
+///	jodeWin.action(e);
+///	return true;
+///    }
+///#endif
     	
     public void init() {
 	String cp = getParameter("classpath");
@@ -20,3 +28,4 @@ public class JodeApplet extends Applet {
 	    jodeWin.setClass(cls);
     }
 }
+
