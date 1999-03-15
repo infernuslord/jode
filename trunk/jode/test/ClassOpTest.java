d39c2d0709c948b94241260aeead58c20963fe77
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-03-15 21:28:58 +0000

    *** empty log message ***

--- trunk/jode/test/ClassOpTest.java
+++ trunk/jode/test/ClassOpTest.java
@@ -2,7 +2,7 @@ package jode.test;
 
 public class ClassOpTest {
     static void test1() {
-	Class c1 = ClassOpTest.class;
+	Class c1 = String.class;
 	Class c2 = Object.class;
 	if (ClassOpTest.class == null);
 	c1.getClass();
