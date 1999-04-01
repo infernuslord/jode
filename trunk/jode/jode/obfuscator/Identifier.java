ae9fa68fa730b0edeb8a136ae7049be0add81233
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-04-01 10:33:22 +0000

    added analyze method

--- trunk/jode/jode/obfuscator/Identifier.java
+++ trunk/jode/jode/obfuscator/Identifier.java
@@ -1,21 +1,22 @@
-/* 
- * Identifier (c) 1998 Jochen Hoenicke
+/* Identifier Copyright (C) 1999 Jochen Hoenicke.
  *
- * You may distribute under the terms of the GNU General Public License.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- * IN NO EVENT SHALL JOCHEN HOENICKE BE LIABLE TO ANY PARTY FOR DIRECT,
- * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF
- * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JOCHEN HOENICKE 
- * HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * JOCHEN HOENICKE SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
- * BASIS, AND JOCHEN HOENICKE HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
- * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Id$
  */
+
 package jode.obfuscator;
 import jode.Obfuscator;
 import java.io.*;
@@ -34,10 +35,11 @@ public abstract class Identifier {
 
     private String alias = null;
     private boolean wasAliased = false;
-
+    
     public Identifier(String alias) {
 	this.alias = alias;
     }
+
 
     /**
      * Returns true, if this identifier is reachable in some way, false if it
@@ -46,6 +48,7 @@ public abstract class Identifier {
     public final boolean isReachable() {
 	return reachable;
     }
+
     /**
      * true, if this identifier must preserve its name, false if the
      * name may be obfuscated.
@@ -229,4 +232,11 @@ public abstract class Identifier {
     public abstract String getFullName();
     public abstract String getFullAlias();
     public abstract boolean conflicting(String newAlias, boolean strong);
+
+    /**
+     * This is called by ClassBundle when it a class is added with
+     * ClassBundle.analyzeIdentifier().
+     */
+    public void analyze() {
+    }
 }
