55fb8bea2ec43f462e4e4125ff267c32aa29b7ef
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-05-05 21:01:36 +0000

    static classes added

--- jode/test/InnerCompat.java
+++ jode/test/InnerCompat.java
@@ -1,3 +1,22 @@
+/* InnerCompat Copyright (C) 1999 Jochen Hoenicke.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
 package jode.test;
 
 public class InnerCompat {
@@ -32,4 +51,20 @@ public class InnerCompat {
 	void a() { x = 5; }
     }
 
+    private static class privateStatic {
+	int x;
+	void a() { x = 5; }
+    }
+    protected static class protectedStatic {
+	int x;
+	void a() { x = 5; }
+    }
+    static class packageStatic {
+	int x;
+	void a() { x = 5; }
+    }
+    public static class publicStatic {
+	int x;
+	void a() { x = 5; }
+    }
 }
