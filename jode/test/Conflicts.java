c702cda8bc683e05822184fcb7179660c378980b
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-05-05 21:00:23 +0000

    Much more evil things added ;-) Does only compile under jikes, not javac :-(

--- jode/test/Conflicts.java
+++ jode/test/Conflicts.java
@@ -1,13 +1,66 @@
+/* Conflicts Copyright (C) 1999 Jochen Hoenicke.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
 package jode.test;
 
 /**
- * This class tests name conflicts and their resolvation.
+ * This class tests name conflicts and their resolvation.  Note that every
+ * name in this file should be the shortest possible name.
  */
-public class Conflicts {
+public class Conflicts
+{
     int Conflicts;
+
+    class Blah
+    {
+	Conflicts Inner;
+	
+	void Conflicts() {
+	    Inner = jode.test.Conflicts.this;
+	}
+    }
     
-    class Inner {
+    class Inner
+    {
 	int Conflicts;
+	Conflicts Inner;
+
+	class Blah 
+	    extends Conflicts.Blah 
+	{
+	    int Blah;
+	    
+	    void Inner() {
+		this.Inner.Inner();
+		jode.test.Conflicts.Inner.this.Inner.Inner();
+		this.Conflicts();
+		jode.test.Conflicts.Inner.this.Conflicts();
+	    }
+
+	    Blah() {
+		/* empty */
+	    }
+	    
+	    Blah(Conflicts Conflicts) {
+		Conflicts.super();
+	    }
+	}
 
 	void Conflicts() {
 	    int Conflicts = 4;
@@ -22,23 +75,33 @@ public class Conflicts {
 	    jode.test.Conflicts.this.Conflicts = this.Conflicts;
 	}
 
-	jode.test.Conflicts Conflicts(Inner Conflicts) {
+	Conflicts Conflicts(Inner Conflicts) {
+	    Conflicts.this$0.Conflicts = 9;
 	    return jode.test.Conflicts.this;
 	}
     }
 
+    class Second 
+	extends Conflicts.Inner.Blah 
+    {
+	Inner Blah = new Inner();
+
+	class Inner extends Conflicts.Inner
+	{
+	}
+
+	Conflicts.Inner create() {
+	    return jode.test.Conflicts.this.new Inner();
+	}
+
+	Second(Conflicts.Inner Blah) {
+	    Blah.super();
+	}
+    }
+
     public void Inner() {
+	/* empty */
     }
-
-    class Second {
-	class Inner {
-	}
-	void create() {
-	    new Inner();
-	    jode.test.Conflicts.this.new Inner();
-	}
-    }
-
     
     public Conflicts() {
 	int Conflicts = this.Conflicts;
@@ -47,4 +110,3 @@ public class Conflicts {
 	new Inner().Conflicts(Inner).Inner();
     }
 }
-
