bb00a0e51a5602dba24445d8273ccbe038d7b3e6
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-06-18 12:09:37 +0000

    long/double tests added some more tests

--- trunk/jode/test/AnonymousJavac.java
+++ trunk/jode/test/AnonymousJavac.java
@@ -26,8 +26,10 @@ public class AnonymousJavac {
 	int var = 3;
 
 	public void test() {
+	    final long longVar = 5;
+	    final double dblVar = 3;
 	    class Hello {
-		int var = 4;
+		int var = (int) longVar;
 
 		Hello() {
 		    System.err.println("construct");
@@ -40,6 +42,85 @@ public class AnonymousJavac {
 		    this.hashCode();
 		    Inner.this.hashCode();
 		    Inner.this.var = var;
+		    AnonymousJavac.this.hashCode();
+		    System.err.println("HelloWorld: "+dblVar);
+		}
+	    };
+	    final Hello hi = new Hello();
+	    final Hello ho = new Hello("ho");
+	    final Object o = new Object() {
+		int blah = 5;
+		Hello hii = hi;
+
+		public String toString() {
+		    this.hii.hello();
+		    hi.hello();
+		    return Integer.toHexString(AnonymousJavac.this.hashCode()
+					       +blah);
+		}
+	    };
+	    Object p = new Object() {
+		public String toString() {
+		    return o.toString();
+		}
+	    };
+//  	    Hello blah = new Hello("Hello World") {
+//  		public void hello() {
+//  		    System.err.println("overwritten");
+//  		}
+//  	    };
+
+	    Inner blub1 = new Inner("Inner param") {
+		Hello hii = hi;
+
+		public void test() {
+		    System.err.println("overwritten: "+hii+hi);
+		}
+	    };
+
+	    Inner blub2 = new AnonymousJavac().new Inner("Inner param") {
+		Hello hii = hi;
+
+		public void test() {
+		    System.err.println("overwritten: "+hii);
+		    AnonymousJavac.this.hashCode();
+		}
+	    };
+
+	    class Hi extends Inner {
+		public Hi() {
+		    super("Hi World");
+		}
+	    }
+
+	    Vector v = new Vector(hi.var, var) {
+		Hello hii = hi;
+		public Object clone() {
+		    return super.clone();
+		}
+	    };
+
+	    Hi hu = new Hi();
+		
+	}
+	Inner (String str) {
+	}
+    }
+
+
+	public void test() {
+	    class Hello {
+		int var = 4;
+
+		Hello() {
+		    System.err.println("construct");
+		}
+		Hello(String info) {
+		    System.err.println("construct: "+info);
+		}
+
+		public void hello() {
+		    this.hashCode();
 		    AnonymousJavac.this.hashCode();
 		    System.err.println("HelloWorld");
 		}
@@ -86,17 +167,14 @@ public class AnonymousJavac {
 		}
 	    }
 
-	    Vector v = new Vector(hi.var, var) {
-		public String toString() {
-		    return super.toString();
+	    Vector v = new Vector(hi.var, new Inner("Hi").var) {
+		public Object clone() {
+		    return super.clone();
 		}
 	    };
 
 	    Hi hu = new Hi();
 		
 	}
-	Inner (String str) {
-	}
-    }
 
 }
