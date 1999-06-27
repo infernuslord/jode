afa9bcc987b0f3fce006182557ce431e6d8cf40b
jochen <jochen@379699f6-c40d-0410-875b-85095c16579e>
1999-06-27 21:05:34 +0000

    Initial revision

---
+++ jode/jode/util/SortedMap.java
@@ -0,0 +1,38 @@
+// This interface is taken from the Classpath project.  
+// Please note the different copyright holder!  
+// The changes I did is this comment, the package line, some
+// imports from java.util and some minor jdk12 -> jdk11 fixes.
+// -- Jochen Hoenicke <jochen@gnu.org>
+
+/////////////////////////////////////////////////////////////////////////////
+// SortedMap.java -- A map that makes guarantees about the order of its keys
+//
+// Copyright (c) 1998 by Stuart Ballard (stuart.ballard@mcmail.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Library General Public License as published
+// by the Free Software Foundation, version 2. (see COPYING.LIB)
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public License
+// along with this program; if not, write to the Free Software Foundation
+// Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+/////////////////////////////////////////////////////////////////////////////
+
+// TO DO:
+// ~ Doc comments for everything.
+
+package jode.util;
+
+public interface SortedMap extends Map {
+  Comparator comparator();
+  Object firstKey();
+  SortedMap headMap(Object toKey);
+  Object lastKey();
+  SortedMap subMap(Object fromKey, Object toKey);
+  SortedMap tailMap(Object fromKey);
+}
