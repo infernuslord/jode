6866570b24e815756d493d5dfb7ba5b84104edad
hoenicke <hoenicke@379699f6-c40d-0410-875b-85095c16579e>
2001-08-16 08:55:21 +0000

    Copied documentation from Jode-1.1

--- trunk/jode/doc/download.php
+++ trunk/jode/doc/download.php
@@ -1,41 +1,34 @@
 <?php require("header.inc"); ?>
 <h1>Download</h1>
 
-<p>Click <?php sflink("project/filelist.php")?>here</a> to download
-the latest released source code of <i>JODE</i> .  You need several
-other packages to build <i>JODE</i>, check the <?php selflink("links")
-?>links page</a>. </p>
+<p>Jode is available in the <?
+sflink("project/showfiles.php")?>download area</a> in source or
+binary form.  For compiling the source code, you need several other
+packages, check the <?php selflink("links") ?>links page</a>.  You
+need a unix like environment for compilation.</p>
 
 <p>The simplest way to get it, especially for non unix users, is in
-precompiled form, though.  I have two jar archives at the <a
-href="ftp://jode.sourceforge.net/pub/jode">ftp server</a>.  You may
-need to press shift while clicking on the link, depending on your
-browser.
+precompiled form, though.  There are two jar archives in the download
+area:</P>
 
-<?php
-function jarlink($what) {
-  global $version;
-  echo "<a href=\"ftp://jode.sourceforge.net/pub/jode/jode-".$version.$what;
-  echo ".jar\">jode-".$version.$what.".jar</a>";
-} ?>
+<ul> <li>jode-1.1-JDK1.1.jar is for JDK&nbsp;1.1.  If you want to use
+the swing interface, you have to download swing separately, all other
+packages are already included in the archive. </li>
 
-<ul> <li><?php jarlink("-1.1") ?> is for JDK&nbsp;1.1.  It contains
-the collection classes from the GNU Classpath project.  If you want to
-use the swing interface, you have to download swing separately. </li>
-
-<li> <?php jarlink("-1.2") ?> is for JDK&nbsp;1.2 or better. </li> </ul>
-</p>
+<li>jode-1.1.jar is for JDK&nbsp;1.2 or better.  It should run
+without any other package.</li> </ul> </p>
 
 <h1>CVS Repository</h1>
 
 <p>You can get the latest sources from the <?php sflink("cvs/") ?> CVS
-repository</a>.  They may not always compile, though.  If you want an
-older version you can use the <code>-r</code> option:</p>
+repository</a>.  Follow the instruction on that page; use
+<code>jode</code> as <i>modulename</i>.  If you want to checkout a
+specific version you can use the <code>-r</code> option:</p> 
+
 <ul>
-<li><code>-r jode_1_0_93</code>: gives you the version 1.0.93</li>
-<li><code>-r branch_1_1</code>: gives you the latest version that will
-become 1.1</li>
-</ul>
+<li><code>-r jode_1_0_93</code>: checks out the version 1.0.93</li>
+<li><code>-r branch_1_1</code>: checks out the latest version in the
+1.1 series.</li> </ul>
 
 <p>To build the sources from CVS change to the main directory where
 the <code>configure.in</code> file resides and run
@@ -43,5 +36,5 @@ the <code>configure.in</code> file resides and run
 <pre>aclocal && automake -a && autoconf</pre>
 
 Afterwards follow the instruction in the INSTALL file.  </p>
-
 <?php require("footer.inc"); ?>
+
--- trunk/jode/doc/feedback.php
+++ trunk/jode/doc/feedback.php
@@ -2,16 +2,13 @@
 
 <h1>Feedback</h1>
 
-<p>You can report bugs to the <?php sflink("bugs/")?>bug forum</a>.
-Please send me a short notice if you add a bug.</p>
+<p>You can report bugs to the <?php sflink("bugs/")?>bug forum</a>. </p>
 
-<p>You can contact me per email via <a
+<p>You can contact me by email via <a
 href="http://sourceforge.net/sendmessage.php?touser=18252">hoenicke at
-users.sourceforge.net</a>.  Please mention <i>Jode</i> in the
+users.sourceforge.net</a>.  Please mention <i>jode</i> in the
 subject.</p>
 
 <p>There is a mailing list.  Check <a href="http://lists.sourceforge.net/mailman/listinfo/jode-users">this page</a> for subscription informations.</p>
-
-
 
 <?php require("footer.inc"); ?> 
