18d38951fe65e9a76d72b658ff793c1be7698e40
hoenicke <hoenicke@379699f6-c40d-0410-875b-85095c16579e>
2004-06-01 08:46:10 +0000

    Set copyright to LGPL for package type

--- jode/src/net/sf/jode/type/RangeType.java
+++ jode/src/net/sf/jode/type/RangeType.java
@@ -1,17 +1,17 @@
 /* RangeType Copyright (C) 1998-2002 Jochen Hoenicke.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; see the file COPYING.LESSER.  If not, write to
  * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Id$
